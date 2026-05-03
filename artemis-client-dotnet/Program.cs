1e63abd5a43ab790cfb826be07bba204d1c871c0
Christian van r <christianvanr@gmail.com>
2026-05-03 11:23:42 +0200

    Added artemis testclient for dotnet and apmq

---
+++ artemis-client-dotnet/Program.cs
@@ -0,0 +1,31 @@
+﻿using Apache.NMS;
+using Apache.NMS.AMQP;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string brokerUri = "amqp://192.168.2.25:5672";
+
+        IConnectionFactory factory = new NmsConnectionFactory(brokerUri);
+        using (IConnection connection = factory.CreateConnection("admin", "admin"))
+        {
+            connection.Start();
+
+            using (ISession session = connection.CreateSession())
+            {
+                IDestination destination = session.GetQueue("test");
+
+                using (IMessageConsumer consumer = session.CreateConsumer(destination))
+                {
+                    IMessage message = consumer.Receive();
+
+                    if (message is ITextMessage textMessage)
+                    {
+                        Console.WriteLine("Ontvangen: " + textMessage.Text);
+                    }
+                }
+            }
+        }
+    }
+}
